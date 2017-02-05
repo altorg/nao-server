2911b0b2bfd6cd1663d03a92444da6688b5c3262
alt.org <nethack@alt.org>
2017-02-05 21:00:59 +0000

    Changed variable name to PROFILE for pl.

--- init/nao_profile.pl
+++ init/nao_profile.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl 
 
-$sourcefile = "/etc/unsafe_profile.sh"; # or whatever
+$sourcefile = "/opt/etc/nao_profile.sh"; 
 chomp(@newenv = qx ( . $sourcefile; env) );
 foreach (@newenv) {
     ($k,$v) = split "=",$_,2;
-    $ENV{$k}=$v;
+    $PROFILE{$k}=$v;
 } 
