5406f780d3bfb32be22142312f941e1516e34c61
alt.org <nethack@alt.org>
2017-02-06 00:26:20 +0000

    bison

--- os/install_packages.sh
+++ os/install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # nethack/dgl build
-apt-get install autoconf autogen gcc
+apt-get install autoconf autogen bison gcc
 # webserver
 apt-get install apache2 mysql-server
 # 32 bit compat for old saves
