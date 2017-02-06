cd592ea75931bc0b55c39da3ccae8f818ea39c87
alt.org <nethack@alt.org>
2017-02-06 00:41:16 +0000

    added libncurses

--- os/install_packages.sh
+++ os/install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # nethack/dgl build
-apt-get install autoconf autogen bison gcc libncursesw5-dev make
+apt-get install autoconf autogen bison gcc libncurses5-dev libncursesw5-dev make
 # webserver
 apt-get install apache2 mysql-server
 # 32 bit compat for old saves
