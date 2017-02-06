f7899d86efafda3ba4acadc883d7fef7a408b7b8
alt.org <nethack@alt.org>
2017-02-06 04:43:58 +0000

    inetd and telnetd-ssl

--- os/install_packages.sh
+++ os/install_packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # nethack/dgl build
 apt-get install autoconf autogen bison flex gcc libncurses5-dev libncursesw5-dev libsqlite3-dev make sqlite3
+# os running
+apt-get install telnetd-ssl
 # webserver
 apt-get install apache2 mysql-server
 # 32 bit compat for old saves
