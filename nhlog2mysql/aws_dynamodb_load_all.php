0ed3a0a8887cccbef9e6657b692bcc438647b788
alt.org <nethack@alt.org>
2017-03-29 05:24:12 +0000

    aws dynamo db works now, although it is the wrong application and I won't use this

--- nhlog2mysql/aws_dynamodb_load_all.php
+++ nhlog2mysql/aws_dynamodb_load_all.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require ('/usr/share/php5/aws.phar');
 
 date_default_timezone_set('UTC');
 
@@ -7,45 +7,44 @@ use Aws\DynamoDb\Exception\DynamoDbException;
 use Aws\DynamoDb\Marshaler;
 
 $sdk = new Aws\Sdk([
-    'endpoint'   => 'http://localhost:8000',
-    'region'   => 'us-west-2',
+    'region'   => 'us-east-1',
     'version'  => 'latest'
 ]);
 
 $dynamodb = $sdk->createDynamoDb();
 $marshaler = new Marshaler();
+$xlogfile_file = "/opt/nethack/nethack.alt.org/nh360/var/xlogfile";
 
-$tableName = 'Movies';
+$tableName = 'altorg-nhlogs';
 
-$movies = json_decode(file_get_contents('moviedata.json'), true);
-
-foreach ($movies as $movie) {
-
-    $year = $movie['year']; 
-    $title = $movie['title'];
-    $info = $movie['info'];
-
-    $json = json_encode([
-        'year' => $year,
-        'title' => $title,
-        'info' => $info
-    ]);
-
-    $params = [
-        'TableName' => $tableName,
-        'Item' => $marshaler->marshalJson($json)
-    ];
-
-    try {
-        $result = $dynamodb->putItem($params);
-        echo "Added movie: " . $movie['year'] . " " . $movie['title'] . "\n";
-    } catch (DynamoDbException $e) {
-        echo "Unable to add movie:\n";
-        echo $e->getMessage() . "\n";
-        break;
+$handle = fopen($xlogfile_file, "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        $xlogfile_entry = array();
+        $pairs = explode("\t", $line);
+	foreach($pairs as $pair) {
+            list ($key,$value) = explode("=", $pair);
+            $xlogfile_entry[trim($key)] = trim($value);
+        }
+        $xlogfile_entry['logid'] = md5($xlogfile_entry['name'].$xlogfile_entry['starttime']);
+        $json = json_encode($xlogfile_entry);
+        // print $json."\n";
+        $params = [
+          'TableName' => $tableName,
+          'Item' => $marshaler->marshalJson($json)
+        ];
+        try {
+            $result = $dynamodb->putItem($params);
+        } catch (DynamoDbException $e) {
+            echo "Unable to add dynamodb entry:\n";
+            echo $e->getMessage() . "\n";
+            break;
+        }
     }
-
-}
+    fclose($handle);
+} else {
+    exit("Can't open xlogfile.");
+} 
 
 ?>
 
